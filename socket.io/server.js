cb8f3e79979a770fd831bc89e2ea607b71b2fca1
Outsider <outsideris@gmail.com>
2010-10-18 03:40:02 +0900

    httpserver handle static file

--- socket.io/server.js
+++ socket.io/server.js
@@ -1,15 +1,32 @@
 var http = require("http"),
     sys = require("sys"),
+    url = require("url"),
+    fs = require("fs"),
     io = require("socket.io"),
     PORT = 8124,
     HOST = null,
 
 server = http.createServer(function (req, res) {
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.write("<h1>Hello World</h1>");
-    res.end();
+
+    //util.get('/', util.staticHandler('index.html'));
+    var path = url.parse(req.url).pathname;
+    sys.debug(path);
+    switch (path) {
+        case '/':
+        case '/main.html':
+            staticHandler("../main.html", req, res);
+            break;
+        case '/main.js':
+            staticHandler("../main.js", req, res);
+            break;
+        default:
+            res.writeHead(404);
+            res.write("404");
+            res.end();
+    }
 });
 server.listen(PORT, HOST);
+
 
 var socket = io.listen(server);
 
@@ -22,5 +39,23 @@ socket.on("connect", function(client) {
       sys.debug("disconnect");
     });
 });
+
+var staticHandler = function(filename, req, res) {
+	var body;
+	
+    fs.readFile(filename, function(err, data) {
+	    if (err) {
+		    sys.debug('Error loading file ' + filename);
+	    } else {
+	    	body = data;
+	    }
+        res.writeHead(200, {
+			'Content-Type': 'text/html',
+			'Content-Length': body.length
+		});
+		res.write(body);
+		res.close();
+	});
+};
 
 sys.debug("Server running at http://localhost:" + PORT);
